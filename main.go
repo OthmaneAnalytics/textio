a4990c8d0f3ef4b4892ede4d24992b2ef9b50b84
marbel <othmane.xx90@gmail.com>
2024-11-29 22:07:04 +0100

    variables

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello there!")
+	var smsSendingLimit int
+	smsSendingLimit = 0
+	var costPerSMS float64
+	costPerSMS = 0
+	var hasPermission bool
+	hasPermission = false
+	var username string
+	username = ""
+	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
 
