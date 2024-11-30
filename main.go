fba1ee133e8b62880a5ec7586f77a053364f4f38
marbel <othmane.xx90@gmail.com>
2024-11-30 20:05:10 +0100

    computed constants

--- main.go
+++ main.go
@@ -37,4 +37,10 @@ func main() {
 
 	fmt.Println("plan:", premiumPlanName)
 	fmt.Println("plan:", basicPlanName)
+	const secondsInMinute = 60
+	const minutesInHour = 60
+	const secondsInHour = secondsInMinute * minutesInHour
+
+	// don't edit below this line
+	fmt.Println("number of seconds in an hour:", secondsInHour)
 }
