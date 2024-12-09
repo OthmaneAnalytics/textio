96d9474fac2489fe27d1ee70900c8fc5fa9fc61d
marbel <othmane.xx90@gmail.com>
2024-12-09 10:08:47 +0100

    multiple interfaces

--- main.go
+++ main.go
@@ -6,6 +6,34 @@ import (
 	"time"
 )
 
+func (e email) cost() int {
+	if e.isSubscribed{
+		return 2*len(e.body)
+	}
+	return 5*len(e.body)
+}
+
+func (e email) format() string {
+	if e.isSubscribed {
+		return "'"+ e.body +"'" + " | " + "Subscribed"
+	}
+	return "'"+ e.body +"'" + " | " + "Not Subscribed"
+}
+
+type expense interface {
+	cost() int
+}
+
+type formatter interface {
+	format() string
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+}
+
+
 type employee interface {
 	getName() string
 	getSalary() int
