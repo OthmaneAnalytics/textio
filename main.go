b238791a9b35846156119aff67c559a68171e3fe
marbel <othmane.xx90@gmail.com>
2024-12-09 09:47:08 +0100

    interface implementation

--- main.go
+++ main.go
@@ -6,6 +6,39 @@ import (
 	"time"
 )
 
+type employee interface {
+	getName() string
+	getSalary() int
+}
+
+type contractor struct {
+	name         string
+	hourlyPay    int
+	hoursPerYear int
+}
+
+func (c contractor) getSalary() int{
+	return c.hourlyPay * c.hoursPerYear
+}
+
+func (c contractor) getName() string {
+	return c.name
+}
+
+type fullTime struct {
+	name   string
+	salary int
+}
+
+func (ft fullTime) getSalary() int {
+	return ft.salary
+}
+
+func (ft fullTime) getName() string {
+	return ft.name
+}
+
+
 func sendMessage(msg message) (string, int) {
 	content := msg.getMessage()
 	l := len(content)
