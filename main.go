7d9e49b914140f29cd40b3bf89f984284dced22b
marbel <othmane.xx90@gmail.com>
2024-12-09 09:40:24 +0100

    interfaces in go

--- main.go
+++ main.go
@@ -3,7 +3,39 @@ package main
 import (
 	"unicode/utf8"
 	"fmt"
+	"time"
 )
+
+func sendMessage(msg message) (string, int) {
+	content := msg.getMessage()
+	l := len(content)
+	return content, 3*l
+}
+
+type message interface {
+	getMessage() string
+}
+
+// don't edit below this line
+
+type birthdayMessage struct {
+	birthdayTime  time.Time
+	recipientName string
+}
+
+func (bm birthdayMessage) getMessage() string {
+	return fmt.Sprintf("Hi %s, it is your birthday on %s", bm.recipientName, bm.birthdayTime.Format(time.RFC3339))
+}
+
+type sendingReport struct {
+	reportName    string
+	numberOfSends int
+}
+
+func (sr sendingReport) getMessage() string {
+	return fmt.Sprintf(`Your "%s" report is ready. You've sent %v messages.`, sr.reportName, sr.numberOfSends)
+}
+
 
 type contact struct {
 	sendingLimit int32
--- main_test.go
+++ main_test.go
@@ -2,51 +2,59 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
+	"time"
 )
 
 func Test(t *testing.T) {
 	type testCase struct {
-		name     string
-		expected uintptr
+		msg          message
+		expectedText string
+		expectedCost int
 	}
-
 	tests := []testCase{
-		{"contact", uintptr(24)},
+		{birthdayMessage{time.Date(1994, 03, 21, 0, 0, 0, 0, time.UTC), "John Doe"},
+			"Hi John Doe, it is your birthday on 1994-03-21T00:00:00Z",
+			168,
+		},
+		{sendingReport{"First Report", 10},
+			`Your "First Report" report is ready. You've sent 10 messages.`,
+			183,
+		},
 	}
 	if withSubmit {
-		tests = append(tests, testCase{"perms", uintptr(16)})
+		tests = append(tests, []testCase{
+			{birthdayMessage{time.Date(1934, 05, 01, 0, 0, 0, 0, time.UTC), "Bill Deer"},
+				"Hi Bill Deer, it is your birthday on 1934-05-01T00:00:00Z",
+				171,
+			},
+			{sendingReport{"Second Report", 20},
+				`Your "Second Report" report is ready. You've sent 20 messages.`,
+				186,
+			},
+		}...)
 	}
 
 	passCount := 0
 	failCount := 0
 
 	for _, test := range tests {
-		var typ reflect.Type
-		if test.name == "contact" {
-			typ = reflect.TypeOf(contact{})
-		} else if test.name == "perms" {
-			typ = reflect.TypeOf(perms{})
-		}
-
-		size := typ.Size()
-
-		if size != test.expected {
+		text, cost := sendMessage(test.msg)
+		if text != test.expectedText || cost != test.expectedCost {
 			failCount++
 			t.Errorf(`---------------------------------
-Inputs:     (%v)
-Expecting:  %v bytes
-Actual:     %v bytes
-Fail`, test.name, test.expected, size)
+Inputs:     %+v
+Expecting:  (%v, %v)
+Actual:     (%v, %v)
+Fail`, test.msg, test.expectedText, test.expectedCost, text, cost)
 		} else {
 			passCount++
 			fmt.Printf(`---------------------------------
-Inputs:     (%v)
-Expecting:  %v bytes
-Actual:     %v bytes
+Inputs:     %+v
+Expecting:  (%v, %v)
+Actual:     (%v, %v)
 Pass
-`, test.name, test.expected, size)
+`, test.msg, test.expectedText, test.expectedCost, text, cost)
 		}
 	}
 
