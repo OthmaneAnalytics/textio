3c34fd766f0fe8af448fea34a404894d09e502ed
marbel <othmane.xx90@gmail.com>
2024-12-10 10:18:19 +0100

    error package

--- main.go
+++ main.go
@@ -4,7 +4,15 @@ import (
 	"unicode/utf8"
 	"fmt"
 	"time"
+	"errors"
 )
+
+func divide(x, y float64) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("no dividing by 0")
+	}
+	return x / y, nil
+}
 
 type divideError struct {
 	dividend float64
@@ -14,7 +22,7 @@ func (e divideError) Error() string{
 	return fmt.Sprintf("can not divide %v by zero", e.dividend)
 }
 
-func divide(dividend, divisor float64) (float64, error) {
+func divide2(dividend, divisor float64) (float64, error) {
 	if divisor == 0 {
 		return 0, divideError{dividend: dividend}
 	}
