6ba3b66b4be858fb268eb73e3fb9b72c743cf3d5
marbel <othmane.xx90@gmail.com>
2024-12-10 09:44:31 +0100

    the error interface

--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 	"time"
 )
 
+type divideError struct {
+	dividend float64
+}
+
+func (e divideError) Error() string{
+	return fmt.Sprintf("can not divide %v by zero", e.dividend)
+}
+
+func divide(dividend, divisor float64) (float64, error) {
+	if divisor == 0 {
+		return 0, divideError{dividend: dividend}
+	}
+	return dividend / divisor, nil
+}
+
 func getSMSErrorString(cost float64, recipient string) string {
 	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent",cost ,recipient)
 }
