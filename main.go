9cebaf8039d93186efb3658b9c272376f543afe6
marbel <othmane.xx90@gmail.com>
2024-12-11 10:51:13 +0100

    make

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 	"errors"
 )
 
+func getMessageCosts(messages []string) []float64 {
+	costs := make([]float64,len(messages))
+	for i := 0; i < len(messages); i++ {
+		costs[i] = float64(len(messages[i])) * 0.01
+	}  
+	return costs
+}
+
 const (
 	planFree = "free"
 	planPro  = "pro"
