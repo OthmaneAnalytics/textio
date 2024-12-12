b565c7e1708ba40b29d7a218ca6bd94290c448fe
marbel <othmane.xx90@gmail.com>
2024-12-12 10:31:54 +0100

    count instances

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 )
 
 
+func getNameCounts(names []string) map[rune]map[string]int {
+	
+}
+
 func getCounts(messagedUsers []string, validUsers map[string]int) {
 	for i := 0 ; i < len(messagedUsers) ; i++ {
 		if _, ok := validUsers[messagedUsers[i]]; ok {
