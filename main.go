a933516b397216fc5ee86159ae464dabd595e8bc
marbel <othmane.xx90@gmail.com>
2024-12-14 11:01:40 +0100

    channels

--- main.go
+++ main.go
@@ -8,7 +8,34 @@ import (
 	"strings"
 )
 
+type email struct {
+	body string
+	date time.Time
+}
 
+func checkEmailAge(emails [3]email) [3]bool {
+	isOldChan := make(chan bool)
+
+	go sendIsOld(isOldChan, emails)
+
+	isOld := [3]bool{}
+	isOld[0] = <-isOldChan
+	isOld[1] = <-isOldChan
+	isOld[2] = <-isOldChan
+	return isOld
+}
+
+// don't touch below this line
+
+func sendIsOld(isOldChan chan<- bool, emails [3]email) {
+	for _, e := range emails {
+		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
+			isOldChan <- true
+			continue
+		}
+		isOldChan <- false
+	}
+}
 
 func sendEmail(message string) {
 	go func() {
@@ -28,18 +55,7 @@ func test(message string) {
 
 
 
-func (e *email) setMessage(newMessage string) {
-	e.message = newMessage
-}
-
 // don't edit below this line
-
-type email struct {
-	message     string
-	fromAddress string
-	toAddress   string
-}
-
 
 
 func removeProfanity(message *string) {
--- main_test.go
+++ main_test.go
@@ -2,46 +2,62 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"testing"
+	"time"
 )
 
-func TestSetMessage(t *testing.T) {
+func Test(t *testing.T) {
 	type testCase struct {
-		e          email
-		newMessage string
-		expected   string
+		emails [3]email
+		isOld  [3]bool
 	}
 	tests := []testCase{
-		{
-			email{
-				message:     "My name is Lt. Aldo Raine and I'm putting together a special team, and I need me eight soldiers.",
-				fromAddress: "lt.aldo.raine@mailio.com",
-				toAddress:   "army@mailio.com",
+		{[3]email{
+			{
+				body: "Words are pale shadows of forgotten names. As names have power, words have power.",
+				date: time.Date(2019, 2, 0, 0, 0, 0, 0, time.UTC),
 			},
-			"You just say bingo.",
-			"You just say bingo.",
-		},
-		{
-			email{
-				message:     "Now, if one were to determine what attribute the German people share with a beast, it would be the cunning and the predatory instinct of a hawk.",
-				fromAddress: "col.hans.landa@mailio.com",
-				toAddress:   "lapadite@mailio.com",
+			{
+				body: "It's like everyone tells a story about themselves inside their own head.",
+				date: time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
 			},
-			"What a tremendously hostile world that a rat must endure.",
-			"What a tremendously hostile world that a rat must endure.",
-		},
+			{
+				body: "Bones mend. Regret stays with you forever.",
+				date: time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC),
+			},
+		}, [3]bool{true, false, false}},
+		{[3]email{
+			{
+				body: "Music is a proud, temperamental mistress.",
+				date: time.Date(2018, 0, 0, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "Have you heard of that website Boot.dev?",
+				date: time.Date(2017, 0, 0, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "It's awesome honestly.",
+				date: time.Date(2016, 0, 0, 0, 0, 0, 0, time.UTC),
+			},
+		}, [3]bool{true, true, true}},
 	}
 	if withSubmit {
 		tests = append(tests, []testCase{
-			{
-				email{
-					message:     "Nazi ain't got no humanity. They're the foot soldiers of a Jew-hatin', mass murderin' maniac and they need to be dee-stroyed.",
-					fromAddress: "lt.aldo.raine@mailio.com",
-					toAddress:   "basterds@mailio.com",
+			{[3]email{
+				{
+					body: "I have stolen princesses back from sleeping barrow kings.",
+					date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
 				},
-				"I think this just might be my masterpiece.",
-				"I think this just might be my masterpiece.",
-			},
+				{
+					body: "I burned down the town of Trebon",
+					date: time.Date(2019, 6, 6, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					body: "I have spent the night with Felurian and left with both my sanity and my life.",
+					date: time.Date(2022, 7, 0, 0, 0, 0, 0, time.UTC),
+				},
+			}, [3]bool{true, true, false}},
 		}...)
 	}
 
@@ -49,37 +65,42 @@ func TestSetMessage(t *testing.T) {
 	failCount := 0
 
 	for _, test := range tests {
-		originalMessage := test.e.message
-		test.e.setMessage(test.newMessage)
-		if test.e.message != test.expected {
+		isOld := checkEmailAge(test.emails)
+		if !slices.Equal(isOld[:], test.isOld[:]) {
 			failCount++
 			t.Errorf(`
 ---------------------------------
 Test Failed:
-  inputs:
-    * msg: %v
-    * newMessage: %v
-    * from: %v
-    * to: %v
+  input:
+    * %v | %v
+    * %v | %v
+    * %v | %v
   expected: %v
-  actual: %v
-`, originalMessage, test.newMessage, test.e.fromAddress, test.e.toAddress, test.expected, test.e.message)
+  actual:   %v
+`,
+				test.emails[0].body, test.emails[0].date,
+				test.emails[1].body, test.emails[1].date,
+				test.emails[2].body, test.emails[2].date,
+				test.isOld, isOld)
 		} else {
 			passCount++
 			fmt.Printf(`
 ---------------------------------
 Test Passed:
-  inputs:
-    * msg: %v
-    * newMessage: %v
-    * from: %v
-    * to: %v
+  input:
+    * %v | %v
+    * %v | %v
+    * %v | %v
   expected: %v
-  actual: %v
-`, originalMessage, test.newMessage, test.e.fromAddress, test.e.toAddress, test.expected, test.e.message)
+  actual:   %v
+`,
+				test.emails[0].body, test.emails[0].date,
+				test.emails[1].body, test.emails[1].date,
+				test.emails[2].body, test.emails[2].date,
+				test.isOld, isOld)
 		}
 	}
-
+	fmt.Println("---------------------------------")
 	fmt.Printf("%d passed, %d failed\n", passCount, failCount)
 }
 
