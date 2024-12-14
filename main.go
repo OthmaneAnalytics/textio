869690a6dd8221f07babaf30fa558c63b3907736
marbel <othmane.xx90@gmail.com>
2024-12-14 10:46:10 +0100

    concurrency

--- main.go
+++ main.go
@@ -8,9 +8,24 @@ import (
 	"strings"
 )
 
-func test(text string) {
-	fmt.Println(text)
+
+
+func sendEmail(message string) {
+	go func() {
+		time.Sleep(time.Millisecond * 250)
+		fmt.Printf("Email received: '%s'\n", message)
+	}()
+	fmt.Printf("Email sent: '%s'\n", message)
 }
+
+// Don't touch below this line
+
+func test(message string) {
+	sendEmail(message)
+	time.Sleep(time.Millisecond * 500)
+	fmt.Println("========================")
+}
+
 
 
 func (e *email) setMessage(newMessage string) {
@@ -744,8 +759,9 @@ func main() {
 	)
 	fizzbuzz()
 
-	test("starting Mailio server")
-	test("stopping Mailio server")
 
+	test("Hello there Kaladin!")
+	test("Hi there Shallan!")
+	test("Hey there Dalinar!")
 }
 
