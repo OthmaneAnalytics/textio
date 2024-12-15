4cf837a892ab581351845943cc68b685767f19d5
marbel <othmane.xx90@gmail.com>
2024-12-15 11:05:40 +0100

    constraints

--- main_test.go
+++ main_test.go
@@ -2,61 +2,180 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"testing"
+	"time"
 )
 
-func Test(t *testing.T) {
-	test(
-		t,
-		[]int{1, 2, 3, 4},
-		4,
-	)
-	test(
-		t,
-		[]string{"a", "b", "c", "d"},
-		"d",
-	)
-	if withSubmit {
-		test(
-			t,
-			[]int{},
-			0,
-		)
-		test(
-			t,
-			[]bool{true, false, true, true, false},
-			false,
-		)
+func TestChargeForLineItem(t *testing.T) {
+	type testCase struct {
+		newItem           lineItem
+		oldItems          []lineItem
+		balance           float64
+		expected          []lineItem
+		expectedBalance   float64
+		expectedErrString string
 	}
 
-}
+	tests := []testCase{
+		{
+			newItem: subscription{
+				userEmail: "geralt@rivia.com",
+				startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+				interval:  "yearly",
+			},
+			oldItems: []lineItem{
+				subscription{
+					userEmail: "yen@vengerberg.com",
+					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					interval:  "monthly",
+				},
+				oneTimeUsagePlan{
+					userEmail:        "triss@maribor",
+					numEmailsAllowed: 100,
+				},
+			},
+			balance: 1000.00,
+			expected: []lineItem{
+				subscription{
+					userEmail: "yen@vengerberg.com",
+					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					interval:  "monthly",
+				},
+				oneTimeUsagePlan{
+					userEmail:        "triss@maribor",
+					numEmailsAllowed: 100,
+				},
+				subscription{
+					userEmail: "geralt@rivia.com",
+					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					interval:  "yearly",
+				},
+			},
+			expectedBalance:   750.00,
+			expectedErrString: "",
+		},
+	}
 
-func test[T comparable](t *testing.T, s []T, expected T) {
-	if output := getLast(s); output != expected {
-		t.Errorf(`
+	if withSubmit {
+		tests = append(tests, []testCase{
+			{
+				newItem: subscription{
+					userEmail: "geralt@rivia.com",
+					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					interval:  "yearly",
+				},
+				oldItems: []lineItem{
+					subscription{
+						userEmail: "yen@vengerberg.com",
+						startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+						interval:  "monthly",
+					},
+					oneTimeUsagePlan{
+						userEmail:        "triss@maribor",
+						numEmailsAllowed: 100,
+					},
+				},
+				balance:           200.00,
+				expected:          nil,
+				expectedBalance:   0.0,
+				expectedErrString: "insufficient funds",
+			},
+		}...)
+	}
+
+	passCount := 0
+	failCount := 0
+
+	for _, test := range tests {
+		oldItems := append([]lineItem(nil), test.oldItems...)
+		newItems, newBalance, err := chargeForLineItem(
+			test.newItem,
+			test.oldItems,
+			test.balance,
+		)
+		if (err != nil && err.Error() != test.expectedErrString) ||
+			(err == nil && test.expectedErrString != "") ||
+			!slices.Equal(newItems, test.expected) ||
+			newBalance != test.expectedBalance {
+			failCount++
+			t.Errorf(`
 ---------------------------------
 Test Failed:
-  input:    %v
-  expected: %v
-  actual:   %v
+  newItem:  %v
+  oldItems:
+%v
+  balance:  %v
+  expected items:
+%v
+  expected balance: %v
+  expected error:   %v
+  actual items:
+%v
+  actual balance: %v
+  actual error:   %v
 `,
-			s,
-			expected,
-			output,
-		)
-	} else {
-		fmt.Printf(`
+				test.newItem,
+				sliceWithBullets(oldItems),
+				test.balance,
+				sliceWithBullets(test.expected),
+				test.expectedBalance,
+				test.expectedErrString,
+				sliceWithBullets(newItems),
+				newBalance,
+				err,
+			)
+		} else {
+			passCount++
+			fmt.Printf(`
 ---------------------------------
 Test Passed:
-  input:    %v
-  expected: %v
-  actual:   %v
+  newItem:  %v
+  oldItems:
+%v
+  balance:  %v
+  expected items:
+%v
+  expected balance: %v
+  expected error:   %v
+  actual items:
+%v
+  actual balance: %v
+  actual error:   %v
 `,
-			s,
-			expected,
-			output,
-		)
+				test.newItem,
+				sliceWithBullets(oldItems),
+				test.balance,
+				sliceWithBullets(test.expected),
+				test.expectedBalance,
+				test.expectedErrString,
+				sliceWithBullets(newItems),
+				newBalance,
+				err,
+			)
+		}
 	}
+
+	fmt.Println("---------------------------------")
+	fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+}
+
+func sliceWithBullets[T any](slice []T) string {
+	if slice == nil {
+		return "  <nil>"
+	}
+	if len(slice) == 0 {
+		return "  []"
+	}
+	output := ""
+	for i, item := range slice {
+		form := "  - %v\n"
+		if i == (len(slice) - 1) {
+			form = "  - %v"
+		}
+		output += fmt.Sprintf(form, item)
+	}
+	return output
 }
 
 // withSubmit is set at compile time depending on which button is used to run the tests
